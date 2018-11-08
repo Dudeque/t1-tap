1ed3f3a27d6fd257c69022e0c708460c9ab936d6
Dudeque <mafd17@inf.ufpr.br>
2018-11-08 11:00:03 -0200

    temp commit

---
+++ src/CancelarMensagemComando.java
@@ -0,0 +1,13 @@
+public class CancelarMensagemComando implements Comando {
+	
+	public Mensagem mensagem;
+
+	public CancelarMensagemComando(Mensagem mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public void executar() {
+		//todo
+	}
+
+}
